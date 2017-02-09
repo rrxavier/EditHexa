47abc5bc812bb6002c326567d3a07e62c7732fd6
ricardo.nnsdl <ricardo.nnsdl@eduge.ch>
2017-02-09 15:18:09 +0100

    Script Lecture Fichier
    [COMPLET]

---
+++ ReadFileBits/ReadFileBits/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ReadFileBits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] test = File.ReadAllBytes("C:\\Users\\nunesr_info\\Documents\\GitHub\\EditHexa\\TASKS.txt");
+
+            foreach (byte item in test)
+            {
+                Console.WriteLine(String.Format("{0:X2}", Convert.ToInt32(item)));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

0354899abb79635aea054bc9f24373f572d740be
Lyssei <zoe.cugni@gmail.com>
2017-02-09 16:08:58 +0100

    Affichage des octets fait

---
+++ editeurHexadecimal/editeurHexadecimal/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace editeurHexadecimal {
+    partial class Form1 {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent() {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.btnFileData = new System.Windows.Forms.Button();
+            this.btnHexaData = new System.Windows.Forms.Button();
+            this.detailedDataPanel = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.detailedDataPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(351, 34);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(437, 179);
+            this.dataGridView.TabIndex = 3;
+            // 
+            // btnFileData
+            // 
+            this.btnFileData.Location = new System.Drawing.Point(3, 3);
+            this.btnFileData.Name = "btnFileData";
+            this.btnFileData.Size = new System.Drawing.Size(116, 23);
+            this.btnFileData.TabIndex = 4;
+            this.btnFileData.Text = "Infos sur le fichier";
+            this.btnFileData.UseVisualStyleBackColor = true;
+            // 
+            // btnHexaData
+            // 
+            this.btnHexaData.Location = new System.Drawing.Point(125, 3);
+            this.btnHexaData.Name = "btnHexaData";
+            this.btnHexaData.Size = new System.Drawing.Size(99, 23);
+            this.btnHexaData.TabIndex = 5;
+            this.btnHexaData.Text = "Info sur l\'octet";
+            this.btnHexaData.UseVisualStyleBackColor = true;
+            // 
+            // detailedDataPanel
+            // 
+            this.detailedDataPanel.Controls.Add(this.btnFileData);
+            this.detailedDataPanel.Controls.Add(this.btnHexaData);
+            this.detailedDataPanel.Location = new System.Drawing.Point(22, 78);
+            this.detailedDataPanel.Name = "detailedDataPanel";
+            this.detailedDataPanel.Size = new System.Drawing.Size(304, 314);
+            this.detailedDataPanel.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 646);
+            this.Controls.Add(this.detailedDataPanel);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.detailedDataPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btnFileData;
+        private System.Windows.Forms.Button btnHexaData;
+        private System.Windows.Forms.Panel detailedDataPanel;
+    }
+}
+
---
+++ editeurHexadecimal/editeurHexadecimal/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace editeurHexadecimal {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            string[][] allHexaArray = new string[][] {
+                                        new string[] {"100000", "0","1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"},
+                                        new string[] {"100000", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"},
+                                  };
+
+            DataTable table = new DataTable();
+            List<DataColumn> dataColumnList = new List<DataColumn>();
+            List<DataRow> dataRowList = new List<DataRow>();
+
+            for (int i = 0; i < 17; i++) { 
+                dataColumnList.Add(new DataColumn());
+                dataColumnList.ElementAt(i).DataType = System.Type.GetType("System.String");
+
+                if (i == 0) {
+                    dataColumnList.ElementAt(i).ColumnName = "Offset";
+                } else if (i > 10) {
+                    switch (i) { 
+                        /* all the cases are shifted by 1 because of the offset. This word takes position 0 and shift every number after it by one */
+                        case 11:
+                            dataColumnList.ElementAt(i).ColumnName = "A";
+                            break;
+                        case 12:
+                            dataColumnList.ElementAt(i).ColumnName = "B";
+                            break;
+                        case 13:
+                            dataColumnList.ElementAt(i).ColumnName = "C";
+                            break;
+                        case 14:
+                            dataColumnList.ElementAt(i).ColumnName = "D";
+                            break;
+                        case 15:
+                            dataColumnList.ElementAt(i).ColumnName = "E";
+                            break;
+                        case 16:
+                            dataColumnList.ElementAt(i).ColumnName = "F";
+                            break;
+                    }
+                } else {
+                    dataColumnList.ElementAt(i).ColumnName = (i-1).ToString();
+                }
+                
+                table.Columns.Add(dataColumnList.ElementAt(i));
+            }
+
+            foreach(string[] hexaArray in allHexaArray)
+                table.Rows.Add(hexaArray);
+
+            dataGridView.DataSource = table;
+            dataGridView.Columns[0].Width = 50;
+        }
+    }
+}

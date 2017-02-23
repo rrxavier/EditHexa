1b9bba373823c37e921666f83bcbe38b39b15295
ricardo.nnsdl <ricardo.nnsdl@eduge.ch>
2017-02-23 13:09:28 +0100

    Finalisation de la lecture du fichier en Hexa.

--- ReadFileBits/ReadFileBits/Program.cs
+++ ReadFileBits/ReadFileBits/Program.cs
@@ -11,12 +11,14 @@ namespace ReadFileBits
     {
         static void Main(string[] args)
         {
-            byte[] test = File.ReadAllBytes("C:\\Users\\nunesr_info\\Documents\\GitHub\\EditHexa\\TASKS.txt");
+            /*byte[] test = File.ReadAllBytes("C:\\Users\\nunesr_info\\Documents\\GitHub\\EditHexa\\TASKS.txt");
 
             foreach (byte item in test)
             {
                 Console.WriteLine(String.Format("{0:X2}", Convert.ToInt32(item)));
-            }
+            }*/
+
+            string[][] test = new HexaEditModel("C:\\Users\\nunesr_info\\Documents\\GitHub\\EditHexa\\TASKS.txt").Hexadecimal;
 
             Console.ReadLine();
         }
